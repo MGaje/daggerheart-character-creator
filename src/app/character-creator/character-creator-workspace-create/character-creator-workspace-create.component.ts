e6afe592a4589e782efa4e12875a1dc976779dcd
Michael Gajewski <gajewski.mike@gmail.com>
2024-03-15 19:40:22 +0000

    Step 1 "complete"

--- src/app/character-creator/character-creator-workspace-create/character-creator-workspace-create.component.ts
+++ src/app/character-creator/character-creator-workspace-create/character-creator-workspace-create.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
     selector: 'dh-cc-character-creator-workspace-create',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
     styleUrl: './character-creator-workspace-create.component.scss'
 })
 export class CharacterCreatorWorkspaceCreateComponent {
+    constructor(private readonly fb: FormBuilder) {
+        // Empty.
+    }
 
+    public basicInfoFormGroup: FormGroup = this.fb.group({
+        'name': ['', Validators.required],
+        'pronouns': ['', Validators.required]
+    });
 }
--- src/app/character-creator/character-creator.module.ts
+++ src/app/character-creator/character-creator.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { MatStepperModule } from '@angular/material/stepper';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { LayoutModule, BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 import { CharacterCreatorComponent } from './character-creator/character-creator.component';
@@ -19,9 +23,14 @@ import { CharacterCreatorWorkspaceCreateComponent } from './character-creator-wo
     ],
     imports: [
         CommonModule,
+        FormsModule,
+        ReactiveFormsModule,
         MatStepperModule,
         MatButtonModule,
         MatIconModule,
+        MatDividerModule,
+        MatInputModule,
+        MatFormFieldModule,
         LayoutModule
     ],
     exports: [
